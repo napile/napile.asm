a994f0a62d78a19b488b788ebb0efc59b1d99a1f
VISTALL <vistall@i.ua>
2012-10-31 12:18:54 +0200

    typo

--- src/main/java/org/napile/asm/io/xml/out/AbstractAsmXmlWriter.java
+++ src/main/java/org/napile/asm/io/xml/out/AbstractAsmXmlWriter.java
@@ -502,6 +502,7 @@ public abstract class AbstractAsmXmlWriter<A> extends AsmWriter<Element, Element
 		final Element temp = a.addElement("link_method");
 		Element temp2 = temp.addElement("method");
 		MethodRef methodRef = instruction.methodRef;
+		temp2.addAttribute("name", methodRef.method.toString());
 		methodRef.returnType.accept(this, temp2.addElement("return_type"));
 		ifNotEmptyAdd(methodRef.parameters, "parameters", temp2);
 		ifNotEmptyAdd(methodRef.typeArguments, "type_arguments", temp2);
@@ -514,6 +515,7 @@ public abstract class AbstractAsmXmlWriter<A> extends AsmWriter<Element, Element
 		final Element temp = a.addElement("link_static_method");
 		Element temp2 = temp.addElement("method");
 		MethodRef methodRef = instruction.methodRef;
+		temp2.addAttribute("name", methodRef.method.toString());
 		methodRef.returnType.accept(this, temp2.addElement("return_type"));
 		ifNotEmptyAdd(methodRef.parameters, "parameters", temp2);
 		ifNotEmptyAdd(methodRef.typeArguments, "type_arguments", temp2);
